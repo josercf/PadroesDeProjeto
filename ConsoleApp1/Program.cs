5ae52ebd9ec85b48932e9bf5ddc539ec229fda07
José Romualdo <jose.romualdo@unidas.com.br>
2020-09-16 20:12:49 -0300

    versionar projeto

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPagamento meioPagemento = new ContaCorrente(3_000);
+
+            if (meioPagemento.PossuiSaldo(10_000M))
+                meioPagemento.Debitar(10_000M);
+
+            meioPagemento = new CartaDeCredito();
+            meioPagemento.Debitar(100);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    //Contrato José
+    public interface IPagamento
+    {
+        decimal Saldo { get;  set; }
+        bool PossuiSaldo(decimal valorASerDebitado);
+        void Debitar(decimal valor);
+    }
+
+
+    public class ContaCorrente : IPagamento
+    {
+
+        public ContaCorrente(decimal limite)
+        {
+            LimiteChequeEspecial = limite;
+            Saldo = 1000 + LimiteChequeEspecial;
+        }
+
+
+        public decimal Saldo { get; set; } 
+        public decimal LimiteChequeEspecial { get; set; }
+
+        public void Debitar(decimal valor)
+        {
+            Saldo -= valor;
+        }
+
+        public bool PossuiSaldo(decimal valorASerDebitado)
+        {
+            return Saldo >= valorASerDebitado;
+        }
+    }
+
+
+    public class CartaDeCredito : IPagamento
+    {
+        public decimal Saldo { get; set; } = 1000;
+
+        public void Debitar(decimal valor)
+        {
+            Saldo -= valor;
+        }
+
+        public bool PossuiSaldo(decimal valorASerDebitado)
+        {
+            return Saldo >= valorASerDebitado;
+        }
+    }
+
+    public class Pix : IPagamento
+    {
+        public decimal Saldo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Debitar(decimal valor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PossuiSaldo(decimal valorASerDebitado)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
